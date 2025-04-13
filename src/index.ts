3b450d127780ad9e7a03c60c61772a7de74680df
yuseipen0716 <yuseipen1@gmail.com>
2025-04-13 15:42:47 +0900

    return html

--- src/index.ts
+++ src/index.ts
@@ -7,23 +7,9 @@ const app = new Hono()
 // logger middlewareを追加
 app.use('*', logger())
 
-// 独自のmiddleware
-app.use('/admin/*', async(c, next) => {
-  // 認証checkのシミュレーション
-  const isAuthenticated = c.req.header('X-API-Key') === undefined
-  console.log(c.req.header('X-API-Key'))
-  if (!isAuthenticated) {
-    return c.json({ error: 'Unauthorized' }, 401)
-  }
-  await next()
-})
-
-app.get('/admin/dashboard', (c) => {
-  return c.json({ message: 'Admin dashboard' })
-})
-
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  // return c.text('Hello Hono!')
+  return c.html('<h1>Hello Hono!!</h1>')
 })
 
 serve({
