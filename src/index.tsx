ebd928d590284ad8d4f6ed7854d273f4aa43a403
yuseipen0716 <yuseipen1@gmail.com>
2025-04-13 17:42:50 +0900

    JSX記法を利用してみる

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,44 @@
-import { addAbortListener } from 'events'
+/** @jsx jsx */
+import { jsx } from 'hono/jsx'
 import { Hono } from 'hono'
+import { serve } from '@hono/node-server'
 import type { FC } from 'hono/jsx'
 
 const app = new Hono()
 
 const Layout: FC = (props) => {
+  return (
+    <html>
+      <body>{props.children}</body>
+    </html>
+  )
 }
+
+const Top: FC<{ messages: string[] }> = (props: {
+  messages: string[]
+}) => {
+  return (
+    <Layout>
+      <h1>Hello Hono!</h1>
+      <ul>
+        {props.messages.map((message) => {
+          return <li>{message}!!</li>
+        })}
+      </ul>
+    </Layout>
+  )
+}
+
+app.get('/', (c) => {
+  const messages = ['Good Morning', 'Good Evenint', 'Good Night']
+  return c.html(<Top messages={messages} />)
+})
+
+serve({
+  fetch: app.fetch,
+  port: 3000
+}, (info) => {
+  console.log(`Server is running on http:localhost:${info.port}`)
+})
+
+export default app
