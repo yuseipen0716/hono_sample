ea79a9384c27e6580ee577c19872f73f4fdceb7a
yuseipen0716 <yuseipen1@gmail.com>
2025-04-13 15:41:21 +0900

    middleware sample

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,26 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { logger } from 'hono/logger'
 
 const app = new Hono()
+
+// logger middlewareを追加
+app.use('*', logger())
+
+// 独自のmiddleware
+app.use('/admin/*', async(c, next) => {
+  // 認証checkのシミュレーション
+  const isAuthenticated = c.req.header('X-API-Key') === undefined
+  console.log(c.req.header('X-API-Key'))
+  if (!isAuthenticated) {
+    return c.json({ error: 'Unauthorized' }, 401)
+  }
+  await next()
+})
+
+app.get('/admin/dashboard', (c) => {
+  return c.json({ message: 'Admin dashboard' })
+})
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
